87e833d45c983a4a534ee850c910eeae3cb1ab89
jodafons <jodafons@lps.ufrj.br>
2019-10-15 13:29:20 -0300

    simple setup

--- setup.sh
+++ setup.sh
@@ -1,31 +1,14 @@
-#!/bin/bash
 
-export Athena_SETUP=off
-export CPLUS_INCLUDE_PATH="$CPLUS_INCLUDE_PATH:/usr/include/python2.7/"
-export LC_ALL=''
-export RCM_NO_COLOR=0
-export RCM_GRID_ENV=0
+rm -rf .python_dir
+mkdir .python_dir
+cd .python_dir
+mkdir python
+cd python
+ln -s ../../Gaugi/python Gaugi
+ln -s ../../orchestra/python orchestra
+ln -s ../../External/ringerdb/python ringerdb
 
-export SAPHYRA_PATH=`pwd`
+export PYTHONPATH=`pwd`:$PYTHONPATH
+cd ../../
 
-cd build
-rm -rf lib
-mkdir lib
-for file in "`pwd`"/**/*.pcm
-do
-  echo "ln -sf $file lib"
-  ln -sf $file lib
-done 
-
-for file in "`pwd`"/*.dylib
-do
-  echo "ln -sf $file lib"
-  ln -sf $file lib
-done 
-
-export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
-export PYTHONPATH=`pwd`:$LD_LIBRARY_PATH:$PYTHONPATH
-export PYTHONPATH=`pwd`/python:$PYTHONPATH
-export PATH=`pwd`/scripts:$PATH
-cd ..
 
