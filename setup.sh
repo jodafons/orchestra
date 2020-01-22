72247bebddae3758de63615e10f255d1193bbcb8
jodafons <jodafons@lps.ufrj.br>
2020-01-22 10:19:44 -0300

    update all scripts

--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 
 
-rm -rf .python_dir
-mkdir .python_dir
-cd .python_dir
-
+rm -rf build
+mkdir build
+cd build
 ln -s ../Gaugi/python Gaugi
 ln -s ../orchestra/python orchestra
 export PYTHONPATH=`pwd`:$PYTHONPATH
-cd ../
-
+export PYTHONPATH=`pwd`:$PYTHONPATH
+cd ..
 export PATH=`pwd`/scripts:$PATH
 
 
