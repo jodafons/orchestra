f1bde4ca6b5da656148b92e57add259ccfb5af16
jodafons <jodafons@lps.ufrj.br>
2020-02-18 21:11:55 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,9 +5,7 @@ export RCM_GRID_ENV=0
 rm -rf .__python__
 mkdir .__python__
 cd .__python__
-ln -s ../Gaugi/python Gaugi
 ln -s ../orchestra/python orchestra
-export PYTHONPATH=`pwd`:$PYTHONPATH
 export PYTHONPATH=`pwd`:$PYTHONPATH
 cd ..
 export PATH=`pwd`/scripts:$PATH
