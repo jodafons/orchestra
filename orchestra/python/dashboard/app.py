d2b318fbdaa4492099fe8c25916578445fb15e87
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2020-03-14 13:43:06 -0300

    Will stop for today.

--- orchestra/python/dashboard/app.py
+++ orchestra/python/dashboard/app.py
@@ -70,7 +70,7 @@ class AdminAccessModelView(sqla.ModelView):
         return redirect(url_for('security.login', next=request.url))
 
 class UserView(AdminAccessModelView):
-  column_editable_list = ['email', 'username']
+  column_editable_list = ['email', 'username', 'maxPriority']
   column_searchable_list = column_editable_list
   column_exclude_list = ['password']
   # form_excluded_columns = column_exclude_list
