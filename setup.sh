8e6e3d992814829b4d6dfbbd209b083ab509717b
jodafons <jodafons@lps.ufrj.br>
2020-03-17 18:10:51 -0300

    fix script

--- setup.sh
+++ setup.sh
@@ -2,10 +2,14 @@ export LC_ALL=''
 export RCM_NO_COLOR=0
 export RCM_GRID_ENV=0
 
-rm -rf .__python__
-mkdir .__python__
+if test ! -d "$PWD/.__python__" ; then
+  echo "file __python__ not exist"
+  mkdir .__python__
+fi
+
 cd .__python__
-ln -s ../orchestra/python orchestra
+rm orchestra
+ln -sf ../orchestra/python orchestra
 export PYTHONPATH=`pwd`:$PYTHONPATH
 cd ..
 export PATH=`pwd`/scripts:$PATH
