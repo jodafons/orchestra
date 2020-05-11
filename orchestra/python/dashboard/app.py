1012aef961977c842f157018c0e4729348634f00
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2020-05-11 17:32:42 -0300

    Icons

--- orchestra/python/dashboard/app.py
+++ orchestra/python/dashboard/app.py
@@ -287,7 +287,7 @@ admin = flask_admin.Admin(
 admin.add_view(AdminAccessModelView(Role, db.session, menu_icon_type='fa', menu_icon_value='fa-tags', name="Níveis de acesso"))
 admin.add_view(UserView(Worker, db.session, menu_icon_type='fa', menu_icon_value='fa-users', name="Usuários"))
 admin.add_view(NodeView(Node, db.session, menu_icon_type='fa', menu_icon_value='fa-desktop', name="Nodes"))
-admin.add_view(Rancher_Page(name="Rancher", endpoint='rancher', menu_icon_type='fa', menu_icon_value='fa-hat-cowboy'))
+admin.add_view(Rancher_Page(name="Rancher", endpoint='rancher', menu_icon_type='fa', menu_icon_value='fa-cog'))
 admin.add_view(Grafana_Page(name="Grafana", endpoint='grafana', menu_icon_type='fa', menu_icon_value='fa-line-chart'))
 admin.add_view(QeA_Page(name="FAQ", endpoint='faq', menu_icon_type='fa', menu_icon_value='fa-question'))
 
