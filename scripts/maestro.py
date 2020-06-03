6eac458cc0823183ad5d41cb756d6f184b098a76
jodafons <jodafons@lps.ufrj.br>
2020-06-03 01:54:48 -0300

    fix maestro admin

--- scripts/maestro.py
+++ scripts/maestro.py
@@ -10,11 +10,10 @@ from orchestra.maestro.parsers import DatasetParser, TaskParser
 parser = argparse.ArgumentParser()
 commands = parser.add_subparsers(dest='mode')
 
-
+from partitura import lps
 
 # create the database manager
-db = OrchestraDB(cluster = Cluster.LPS)
-
+db  = OrchestraDB( lps.postgres_url, lps.cluster_volume , cluster=Cluster.LPS  )
 engine = [
             DatasetParser(db, commands),
             TaskParser(db, commands),
