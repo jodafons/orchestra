c7f47d31f52c4c7f00b8607a42c51e8215ad7620
jodafons <jodafons@lps.ufrj.br>
2020-10-12 17:38:51 -0300

    update

--- scripts/maestro.py
+++ scripts/maestro.py
@@ -1,43 +1,55 @@
 #!/usr/bin/env python3
 
-
-if __name__ == '__main__':
-    import sys, os
-    import argparse
-    from orchestra.db import OrchestraDB
-    from orchestra.maestro import PilotParser, NodeParser, UserParser, DatasetParser, TaskParser
-    from orchestra.utils import getConfig
-
-
-    parser = argparse.ArgumentParser()
-    commands = parser.add_subparsers(dest='mode')
-
-
-    config = getConfig()
-
-    # create the database manager
-    db  = OrchestraDB( config['postgres'] )
-
-
-    engine = [
-                PilotParser(db, commands),
-                UserParser(db, commands),
-                NodeParser(db, commands),
-                DatasetParser(db, commands),
-                TaskParser(db, commands),
-              ]
+try:
+  # in case we have root installed here
+  import ROOT
+  from ROOT import gROOT
+  ROOT.PyConfig.IgnoreCommandLineOptions = True
+  gROOT.SetBatch()
+except:
+  pass
 
 
 
-    if len(sys.argv)==1:
-      print(parser.print_help())
-      sys.exit(1)
 
-    args = parser.parse_args()
+import sys, os
+import argparse
+from orchestra.db import OrchestraDB
+from orchestra.maestro import PilotParser, NodeParser, UserParser, DatasetParser, TaskParser
+from orchestra.utils import getConfig
 
-    # Run!
-    for e in engine:
-      e.compile(args)
+from Gaugi import fixArgParser
+fixArgParser()
+
+parser = argparse.ArgumentParser()
+commands = parser.add_subparsers(dest='mode')
+
+
+config = getConfig()
+
+# create the database manager
+db  = OrchestraDB( config['postgres'] )
+
+
+engine = [
+            PilotParser(db, commands),
+            UserParser(db, commands),
+            NodeParser(db, commands),
+            DatasetParser(db, commands),
+            TaskParser(db, commands),
+          ]
+
+
+
+if len(sys.argv)==1:
+  print(parser.print_help())
+  sys.exit(1)
+
+args = parser.parse_args()
+
+# Run!
+for e in engine:
+  e.compile(args)
 
 
 
