c563e64b2f19a2930c1a3ce84a6aec7bdd210213
jodafons <jodafons@lps.ufrj.br>
2020-10-12 18:50:58 +0000

    update docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+current=$PWD
+
+cd $(mktemp -d)
+temp=$PWD
+git clone https://github.com/jodafons/ringer.git
+git clone https://github.com/jodafons/prometheus.git && cd prometheus && mkdir build && cd build && cmake .. && make && cd ../../
+git clone https://github.com/jodafons/orchestra.git
+
+
+
 echo "=========================================================="
 echo "setup root..."
 source /opt/root/buildthis/bin/thisroot.sh
@@ -8,13 +18,15 @@ echo "update all necessary packages..."
 pip install --upgrade Gaugi saphyra
 echo "=========================================================="
 echo "setup prometheus..."
-current=$PWD
-cd /codes/prometheus && git pull && source setup.sh
+cd $temp/prometheus && source setup.sh
 echo "=========================================================="
 echo "setup orchestra..."
-cd /codes/orchestra && git pull && source setup.sh
+cd $temp/orchestra && source setup.sh
+echo "=========================================================="
+echo "setup ringer scripts..."
+cd $temp/ringer && git pull && source setup.sh
+echo "=========================================================="
 
 cd $current
-
 maestro.py
 
