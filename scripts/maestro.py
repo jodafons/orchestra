c9741456aa3e7440f64c513c592a8439c90f9dc4
jodafons <jodafons@lps.ufrj.br>
2020-10-12 19:51:47 +0000

    update

--- scripts/maestro.py
+++ scripts/maestro.py
@@ -1,41 +1,43 @@
 #!/usr/bin/env python3
 
-import sys, os
-import argparse
-from orchestra.db import OrchestraDB
-from orchestra.maestro import PilotParser, NodeParser, UserParser, DatasetParser, TaskParser
-from orchestra.utils import getConfig
+
+if __name__ == '__main__':
+    import sys, os
+    import argparse
+    from orchestra.db import OrchestraDB
+    from orchestra.maestro import PilotParser, NodeParser, UserParser, DatasetParser, TaskParser
+    from orchestra.utils import getConfig
 
 
-parser = argparse.ArgumentParser()
-commands = parser.add_subparsers(dest='mode')
+    parser = argparse.ArgumentParser()
+    commands = parser.add_subparsers(dest='mode')
 
 
-config = getConfig()
+    config = getConfig()
 
-# create the database manager
-db  = OrchestraDB( config['postgres'] )
-
-
-engine = [
-            PilotParser(db, commands),
-            UserParser(db, commands),
-            NodeParser(db, commands),
-            DatasetParser(db, commands),
-            TaskParser(db, commands),
-          ]
+    # create the database manager
+    db  = OrchestraDB( config['postgres'] )
 
 
+    engine = [
+                PilotParser(db, commands),
+                UserParser(db, commands),
+                NodeParser(db, commands),
+                DatasetParser(db, commands),
+                TaskParser(db, commands),
+              ]
 
-if len(sys.argv)==1:
-  print(parser.print_help())
-  sys.exit(1)
 
-args = parser.parse_args()
 
-# Run!
-for e in engine:
-  e.compile(args)
+    if len(sys.argv)==1:
+      print(parser.print_help())
+      sys.exit(1)
+
+    args = parser.parse_args()
+
+    # Run!
+    for e in engine:
+      e.compile(args)
 
 
 
