d65a0e2eb169921d466f95fe008c8b5a05863381
jodafons <jodafons@lps.ufrj.br>
2020-10-19 01:15:22 +0000

    update

--- docker/base/setup_envs.sh
+++ docker/base/setup_envs.sh
@@ -9,19 +9,12 @@ temp=$PWD
 git clone https://github.com/jodafons/saphyra.git
 git clone https://github.com/jodafons/Gaugi.git
 git clone https://github.com/jodafons/ringer.git
-git clone https://github.com/jodafons/prometheus.git && cd prometheus && mkdir build && cd build && cmake .. && make && cd ../../
 git clone https://github.com/jodafons/orchestra.git
 
-echo "=========================================================="
-echo "setup root..."
-source /opt/root/buildthis/bin/thisroot.sh
 echo "=========================================================="
 echo "update all necessary packages..."
 cd $temp/Gaugi && source scripts/setup.sh
 cd $temp/saphyra && source scripts/setup.sh
-echo "=========================================================="
-echo "setup prometheus..."
-cd $temp/prometheus && source setup.sh
 echo "=========================================================="
 echo "setup orchestra..."
 cd $temp/orchestra && source setup.sh
