7a578f7eb4f224bbb9a226dcd7e9980a00b4eede
jodafons <jodafons@lps.ufrj.br>
2022-04-22 15:16:30 -0300

    update

--- share/init_db.sh
+++ share/init_db.sh
@@ -1,14 +1,14 @@
 python ../orchestra/db/create_database.py
 
-maestro.py device create -n caloba21 -d 0 -s 10 -e 1
-maestro.py device create -n caloba21 -d 1 -s 10 -e 1
-maestro.py device create -n caloba22 -d 0 -s 10 -e 1
-maestro.py device create -n caloba22 -d 1 -s 10 -e 1
-maestro.py device create -n caloba23 -d 0 -s 10 -e 1
-maestro.py device create -n caloba23 -d 1 -s 10 -e 1
-maestro.py device create -n caloba24 -d 0 -s 10 -e 1
-maestro.py device create -n caloba24 -d 1 -s 10 -e 1
-maestro.py device create -n caloba25 -d 0 -s 10 -e 1
-maestro.py device create -n caloba25 -d 1 -s 10 -e 1
+maestro.py device create -n caloba21 -d 0 -s 10 -e 2
+maestro.py device create -n caloba21 -d 1 -s 10 -e 2
+maestro.py device create -n caloba22 -d 0 -s 10 -e 2
+maestro.py device create -n caloba22 -d 1 -s 10 -e 2
+maestro.py device create -n caloba23 -d 0 -s 10 -e 2
+maestro.py device create -n caloba23 -d 1 -s 10 -e 2
+maestro.py device create -n caloba24 -d 0 -s 10 -e 2
+maestro.py device create -n caloba24 -d 1 -s 10 -e 2
+maestro.py device create -n caloba25 -d 0 -s 10 -e 2
+maestro.py device create -n caloba25 -d 1 -s 10 -e 2
 
 maestro.py device list
